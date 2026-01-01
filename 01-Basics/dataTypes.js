0a74a7b31ad5bba3a0de2eecfe48d6667ada8f45
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-01 20:25:16 +0530

    Initial commit

---
+++ 01-Basics/dataTypes.js
@@ -0,0 +1,24 @@
+// number => int values 2 to the power of 53 range
+// BigInt => used in trading, big companies
+// string => "" define using '' or ""
+// boolean => true/false
+// undefined => didn't assigned the value yet
+// null => standalone value 
+// symbol => unique 
+
+"use strict";
+
+
+let name = "Siddhesh"
+let age = 21
+let state 
+state = null;
+console.log(state);
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
+
+
