433fc7a60978e9aea48eb780e09bf9aabc6ab3cf
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-01 20:22:31 +0530

    variables

---
+++ 01-Basics/variables.js
@@ -0,0 +1,31 @@
+// let num1 = 20
+// let num2 = 30
+
+// let sum = num1 + num2
+
+// console.log("Hello World")
+// console.log(sum)
+
+const empID = 123
+let empName = "Siddhesh"
+var password = "563" 
+/* 
+Prefer not to use var
+because of issue in the block scope and functional scope
+*/
+empCity = "Jaipur"
+
+let pro;
+
+
+// empID = 236
+// console.log(empCity);
+// console.log(typeof(empCity));
+// console.log(typeof(password));
+
+
+empName = "Ram"
+password = "36562"
+empCity = "Bengalore"
+
+console.table([empID,empName,pro,empCity,password])
