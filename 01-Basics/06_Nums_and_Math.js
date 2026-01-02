d4ab854c089739e5df0cc9d5bc1a60c257566818
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-02 19:55:20 +0530

    Numbers

---
+++ 01-Basics/06_Nums_and_Math.js
@@ -0,0 +1,16 @@
+const num = 500;
+// console.log(num);
+
+const balance = new Number (1000); //[Number: 1000]
+
+// console.log(balance);
+
+const num2 = 123.5623;
+// console.log(num2.toFixed(2)); // 123.56
+// console.log(num2.toPrecision(3)); // 124
+
+const num3 = 10000000;
+console.log(num3.toLocaleString());
+
+
+
