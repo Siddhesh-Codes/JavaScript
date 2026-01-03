d06806de9af6d900cf75afe627abe27505d14f42
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-03 16:16:59 +0530

    Dates and Time

---
+++ 01-Basics/07_Dates_and_Time.js
@@ -0,0 +1,36 @@
+// Dates
+
+const myDate = new Date();
+
+// console.log(myDate.getDate()); // 2 current date 
+// console.log(myDate.getDay()); // 5 current date i.e Friday
+// console.log(myDate.getFullYear()); // 2026 current year
+// console.log(myDate.getTime());
+// console.log(myDate.getMinutes()); // 25 current minute
+// console.log(myDate.getFullYear()); // 2026 current year
+
+// console.log(myDate.toISOString()); 
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString()); // 2/1/2026
+// console.log(myDate.toLocaleString()); // 2/1/2026, 8:29:13 pm
+// console.log(myDate.toString()); //Fri Jan 02 2026 20:29:46 GMT+0530 (India Standard Time)
+// console.log(myDate.toTimeString()); // 20:30:11 GMT+0530 (India Standard Time)
+// console.log(myDate.toUTCString()); //Fri, 02 Jan 2026 15:00:43 GMT
+
+
+const myCreateDate = new Date("01-14-2023");
+// console.log(myCreateDate.toLocaleString());
+
+const myTimeStamp = Date.now();
+
+// console.log(myTimeStamp); // 1767366634423 :- gives values in the miliseconds
+// console.log(myCreateDate.getTime()); // 1673634600000 : :- gives values in the miliseconds
+
+// console.log(Math.floor(Date.now() / 1000));
+
+
+
+
+
+
