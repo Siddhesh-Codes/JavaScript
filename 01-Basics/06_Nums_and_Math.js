b70a7c5a76b7fb30b23c13f142a72fc00c3a824f
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-03 21:11:02 +0530

    03/01 Revision

--- 01-Basics/06_Nums_and_Math.js
+++ 01-Basics/06_Nums_and_Math.js
@@ -5,12 +5,12 @@ const balance = new Number (1000); //[Number: 1000]
 
 // console.log(balance);
 
-const num2 = 123.5623;
+const num2 = 123.623;
 // console.log(num2.toFixed(2)); // 123.56
-// console.log(num2.toPrecision(3)); // 124
+// console.log(num2.toPrecision(3)); // 124 (because after .623 is there).
 
 const num3 = 10000000;
-// console.log(num3.toLocaleString());
+// console.log(num3.toLocaleString()); // 1,00,00,000
 
 
 // ********************* Math ********************* \\
@@ -29,9 +29,21 @@ const min = 100;
 const max = 200;
 
 console.log(Math.floor(Math.random() * (max - min + 1) + min)); 
-// formula to calculate random value when min and max is given. 
-// Here we use floor to get the base value (e.g 10.2 -> 10)
-// then we generate the random values since random function generates values between 0 - 1 
-// we have to multiply it with the (max - min + 1) to get the range e.g 200 - 100 + 1 = 101.
-// then we add min at then end i.e 99 + 100 = 199 
-// so now the random values will get generate between 101 and 199.
+// Formula to calculate a random integer value when min and max are given.
+// Math.random() generates a value between 0 (inclusive) and 1 (exclusive).
+// Multiplying by (max - min + 1) gives a range from 0 up to (max - min + 1) (exclusive).
+// Adding min shifts the range so the minimum possible value is min.
+// Math.floor rounds down, so the maximum possible value is max.
+// For min = 100 and max = 200, this generates values from 100 to 200, inclusive.
+
+
+// Program: Keep generating random numbers until 100 appears
+// let found = false;
+// while (!found) {
+// 	const rand = Math.floor(Math.random() * (max - min + 1) + min);
+// 	console.log(rand);
+// 	if (rand === 200 || rand == 100) {
+// 		console.log('Stopping.');
+// 		found = true;
+// 	}
+// }
