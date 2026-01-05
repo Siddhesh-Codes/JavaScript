6a31e3b8e09fb566bc02291b7a76de95d3bc18b0
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-05 22:14:30 +0530

    Switch Case Statement

---
+++ 04_Control_Flow/02_Switch_Case.js
@@ -0,0 +1,72 @@
+// // Switch Case Syntax:
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "mar";
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2: 
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+//     default:
+//         console.log("Please Enter Valid Month!!");
+//         break;
+// }
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb": 
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    default:
+        console.log("Please Enter Valid Month!!");
+        break;
+}
