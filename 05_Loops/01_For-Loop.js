e54bfa20ee35486c51d2269d3adfe788c8c2a8a0
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-06 19:18:50 +0530

    For Loops Basics

---
+++ 05_Loops/01_For-Loop.js
@@ -0,0 +1,17 @@
+
+let sum = 0; // if we didnt assign any value to the 
+for (let i = 1; i <= 10; i++){
+    sum = sum + i;
+}
+// console.log(sum);
+
+// Print Table in JS
+// Nested For Loop
+for (let i = 1; i <= 10; i++) {
+    
+    for (let j = 1; j <= 10; j++) {
+        // console.log(i + " * " + j + " = " + i * j);
+    } 
+    // console.log();
+}
+
