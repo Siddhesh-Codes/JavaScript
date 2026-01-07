ffa2f69c90355fb60e8f3511124e6f3070497f5f
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-07 22:26:52 +0530

    While and Do While Loop

---
+++ 05_Loops/03_while-do-while.js
@@ -0,0 +1,24 @@
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// Loop through in the array
+// const arr = ["Siddhesh", 1, 21];
+// let i = 0;
+// while (i < arr.length) {
+//     // console.log(arr[i]);
+//     i++;
+// }
+
+
+// do while loop
+let i = 11;
+do {
+    console.log(`Score = ${i}`);
+    
+    // console.log("Do While Loop!");
+    // console.log("It will execute the program at least once. despite the condition is wrong!!");
+    i++
+} while (i <= 10);
