252e0cd9ffc693845aaf595ef90a15bd18b1c8fb
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-10 02:05:24 +0530

    01 - JS Screen Background Color Change.

---
+++ 08-Projects/01-Color-Changer/script.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll('.button');
+
+const body = document.querySelector('body');
+
+buttons.forEach((btn) => {
+  // console.log(btn);
+  btn.addEventListener('click', (event) => {
+    // console.log(event);
+
+    // switch (event.target.id) {
+    //   case 'event.target.id = gray':
+    //     body.style.backgroundColor = 'event.target.id';
+    // }
+
+    if (event.target.id === 'grey') {
+      // we have to assing the value of color not any string.
+      // body.style.backgroundColor = 'event.target.id';
+
+      // Correct Way to do
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'blue') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'yellow') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'white') {
+      body.style.backgroundColor = event.target.id;
+    }
+    // if (event.target.id === 'purple') {
+    //   body.style.backgroundColor = event.target.id;
+    // }
+
+    if (body.style.backgroundColor === 'purple') {
+      body.style.backgroundColor = 'white';
+    } else {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
