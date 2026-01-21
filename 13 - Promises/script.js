51e8c20f31b01e362e45ade440e1fcb23f4882d6
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-01-22 00:31:41 +0530

    Clear Explanation of Async/Await and try-catch block

--- 13 - Promises/script.js
+++ 13 - Promises/script.js
@@ -99,11 +99,58 @@ fun();
 // }
 // getAllUsers();
 
+// Another way to use fetch
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response) => {
+//     return response.json();
+// })
+// .then((response) => {
+//     console.log(response);
+// })
+// .catch((error) => console.log(error));
+
+
+
+// async function getAllUsers(params) {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// getAllUsers();
+
+
+
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => {
     return response.json();
 })
-.then((response) => {
-    console.log(response);
+.then ((data) => {
+    console.log(data);
 })
-.catch((error) => console.log(error));
+.catch((error) => {
+    console.log(error);
+})
+.finally(() => {
+    console.log("Data Fetched Successfully!");
+})
+
+
+
+// Real World Applications 
+
+// 1. Fetching data from the internet (API calls):
+// When you get weather info, news, or user data from a website, you use async/await to wait for the data, and try/catch to handle errors if the internet is down or the server fails.
+
+// 2. Reading or writing files:
+// If your app saves or loads files, async/await lets you wait for the file to finish loading, and try/catch helps if the file is missing or there’s a problem.
+
+// 3. User authentication (login):
+// When a user logs in, async/await waits for the server to check the password, and try/catch handles wrong passwords or server errors.
+
+// 4. Database operations:
+// Async/await is used to wait for the database to respond, and try/catch catches problems like missing data or connection issues.
